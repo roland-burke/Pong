29bea1cdee2abd9d307c8075d7d4ceb0027f266e
Roland Burke <Roland.Burke@gmx.de>
2019-12-25 23:23:17 +0100

    improved game loop

--- src/Game.java
+++ src/Game.java
@@ -34,15 +34,19 @@ public final class Game{
 	private final int ballRadius = 40;
 	
 	
-	public int counter = 0;
-	public double startTimeCalcFPS = System.nanoTime();
+	private final int GAME_HERTZ = 120;
+	private final int SECOND = 1000000000;
+	private int TIME_BETWEEN_UPDATES = SECOND / GAME_HERTZ;
 	
-	private final int FRAME_CAP = 120;
-	private int fps = 0;
-	private final int NANO_SECOND = 1000000000;
-	private int rate = NANO_SECOND / FRAME_CAP;
-	private int milliSleep = rate / 1000000;
-	private int nanoSleep = rate - (milliSleep * 1000000);
+	final int MAX_UPDATES_BEFORE_RENDER = 5;
+    double lastUpdateTime = System.nanoTime();
+    double lastRenderTime = System.nanoTime();
+    
+    final double MAX_FPS = 60;
+    final double TARGET_TIME_BETWEEN_RENDERS = SECOND / MAX_FPS;
+    private int frameCount = 0;
+	
+	private int fps = 60;
 	
 	public Game(JFrame frame) {
 		this.frame = frame;
@@ -65,23 +69,57 @@ public final class Game{
 	}
 	
 	private void run() {
+		//Simple way of finding FPS.
+		int lastSecondTime = (int) (lastUpdateTime / 1000000000);
+		
 		while(running) {
-			if(isF3Pressed) {
-				dp.setInfo("FPS: " + Integer.toString(fps));
-			} else {
-				dp.setInfo("");
-			}
-			if(!paused) {
-				tick();				
-				render();
-			}
-			try {
-				Thread.sleep(milliSleep, nanoSleep);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			calculateFPS();
-		}
+			
+			double now = System.nanoTime();
+	        int updateCount = 0;
+			
+	        //Do as many game updates as we need to, potentially playing catchup.
+            while( now - lastUpdateTime > TIME_BETWEEN_UPDATES && updateCount < MAX_UPDATES_BEFORE_RENDER )
+            {
+            	tick();
+            	lastUpdateTime += TIME_BETWEEN_UPDATES;
+            	updateCount++;
+            }
+			
+            //If for some reason an update takes forever, we don't want to do an insane number of catchups.
+            //If you were doing some sort of game that needed to keep EXACT time, you would get rid of this.
+            if ( now - lastUpdateTime > TIME_BETWEEN_UPDATES)
+            {
+               lastUpdateTime = now - TIME_BETWEEN_UPDATES;
+            }
+         
+            //Render. To do so, we need to calculate interpolation for a smooth render.
+            float interpolation = Math.min(1.0f, (float) ((now - lastUpdateTime) / TIME_BETWEEN_UPDATES) );
+            render(interpolation);
+            lastRenderTime = now;
+         
+            //Update the frames we got.
+            int thisSecond = (int) (lastUpdateTime / 1000000000);
+            if (thisSecond > lastSecondTime)
+            {
+               System.out.println("NEW SECOND " + thisSecond + " " + frameCount);
+               fps = frameCount;
+               frameCount = 0;
+               lastSecondTime = thisSecond;
+            }
+         
+            //Yield until it has been at least the target time between renders. This saves the CPU from hogging.
+            while ( now - lastRenderTime < TARGET_TIME_BETWEEN_RENDERS && now - lastUpdateTime < TIME_BETWEEN_UPDATES)
+            {
+               Thread.yield();
+            
+               //This stops the app from consuming all your CPU. It makes this slightly less accurate, but is worth it.
+               //You can remove this line and it will still work (better), your CPU just climbs on certain OSes.
+               //FYI on some OS's this can cause pretty bad stuttering. Scroll down and have a look at different peoples' solutions to this.
+               try {Thread.sleep(1);} catch(Exception e) {} 
+            
+               now = System.nanoTime();
+            }
+         }
 	}
 	
 	public void start() {
@@ -112,19 +150,17 @@ public final class Game{
 			reset();
 			isRPressed = false;
 		}
-	}
-	
-	private void render() {
-		frame.repaint();
-	}
-	
-	public void calculateFPS() {
-		counter += 1;
-		if(System.nanoTime() - startTimeCalcFPS >= NANO_SECOND) {
-			fps = counter;
-			counter = 0;
-			startTimeCalcFPS = System.nanoTime();
+		
+		if(isF3Pressed) {
+			dp.setInfo("FPS: " + Integer.toString(fps));
+		} else {
+			dp.setInfo("");
 		}
+	}
+	
+	private void render(float interpolation) {
+		
+		dp.repaint();
 	}
 	
 	private void reset() {
